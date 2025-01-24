fda08e44b6eb19fd044669729a8e0d16563ce14c
Maychii <106334255+TanyasiPills@users.noreply.github.com>
2025-01-24 10:24:17 +0100

    i forgor this

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,30 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        
     }
-    
+
+
+    public static List<Versenyzo> Read(String file){
+        List<Versenyzo> list = new ArrayList<Versenyzo>();
+        try(BufferedReader br = new BufferedReader(new FileReader(file))){
+            String line;
+            br.readLine();
+            while((line = br.readLine()) != null){
+                String[] data = line.split(";");
+                data = Arrays.stream(data).map(e -> e.replace(",", ".")).toArray(String[]::new);
+                list.add(new Versenyzo(data));
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return list;
+    }
 }
--- src/Versenyzo.java
+++ src/Versenyzo.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
-
 public class Versenyzo {
+    String name;
+    String orszag;
+    Float technika;
+    Float komponens;
+    int vonas;
+
+    public Versenyzo(String[] data) {
+        this.name = data[0];
+        this.orszag = data[1];
+        this.technika = Float.valueOf(data[2]);
+        this.komponens = Float.valueOf(data[3]);
+        this.vonas = Integer.parseInt(data[4]);
+    }
+
+    @Override
+    public String toString() {
+        return "Fuvar{" +
+                "name: " + name +
+                ", orszag: " + orszag +
+                ", technika: " + technika +
+                ", komponens: " + komponens +
+                ", vonas: " + vonas + '\'' +
+                '}';
+    }
 }
